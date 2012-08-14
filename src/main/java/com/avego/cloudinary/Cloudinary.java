80c07783bdc13e337d609b106da65ac9c24a2f44
Eoghan Cregan <eoghan.cregan@avego.com>
2012-08-14 10:35:23 +0100

    Added Copyright comments to Cloudinary classes.

--- src/main/java/com/avego/cloudinary/Cloudinary.java
+++ src/main/java/com/avego/cloudinary/Cloudinary.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright © 2012 Avego Ltd., All Rights Reserved.
+ * For licensing terms please contact Avego LTD.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.avego.cloudinary;
 
 import java.io.IOException;
